a2e9f2de2cb18ee6cd75f121e54401b617d79bb5
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-12-18 10:39:19 +0100

    add: get size of b-tree

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - get the size of a b-tree
+ * @tree: pointer to root node
+ *
+ * Return: size of b-tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size;
+
+	if (!tree)
+		return (0);
+
+	size = tree->left ? 1 + binary_tree_size(tree->left) : 0;
+	size = tree->right ? 1 + binary_tree_size(tree->right) : 0;
+	size++;
+	return (size);
+}
