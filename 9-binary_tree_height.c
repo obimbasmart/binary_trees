b9aaba41f9381c6b9f321bbc59b5e5c3a1f65ff7
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-12-18 09:13:51 +0100

    add: get height of b-tree

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - get height of b-tree
+ * @tree: pointer to root node
+ *
+ * Return: size of binary tree - height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l_height, r_height;
+
+	l_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	r_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+	return (l_height > r_height ? l_height : r_height);
+}
