71b9f1761b3a40b87e3e16158432b637a2211835
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-12-28 12:18:56 +0100

    fix: incorrect output

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include <stdio.h>
 
 /**
  * binary_tree_balance - get the balance factor of a b_tree
@@ -13,8 +14,29 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	l_height = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
-	r_height = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
+	l_height = get_binary_tree_height(tree->left);
+	r_height = get_binary_tree_height(tree->right);
 
+	printf("left: is %ld\nright is %ld\n", l_height, r_height);
 	return (l_height - r_height);
 }
+
+/**
+ * get_binary_tree_height - get height of b-tree
+ * @tree: pointer to root node
+ *
+ * Return: size of binary tree - height
+ */
+size_t get_binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l_height, r_height;
+
+	if (!tree)
+		return (0);
+
+	l_height = tree->left ? 1 + get_binary_tree_height(tree->left) : 0;
+	r_height = tree->right ? 1 + get_binary_tree_height(tree->right) : 0;
+
+	return (l_height > r_height ? l_height : r_height);
+}
+
