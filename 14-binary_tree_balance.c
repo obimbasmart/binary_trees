ff9d1c5a48681d464a36b821dd3863a43e6196cc
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-12-28 11:28:38 +0100

    fix: logial error

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,8 +13,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	l_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	r_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	l_height = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
+	r_height = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
 
 	return (l_height - r_height);
 }
