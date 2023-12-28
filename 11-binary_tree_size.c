d80f9e0530d090e3132cb1565ebea737d7ca6cc4
obimbasmart <obimbasmartchukwunenye@gmail.com>
2023-12-28 10:00:35 +0100

    new: get size of b_tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include <stdio.h>
 
 /**
  * binary_tree_size - get the size of a b-tree
@@ -8,13 +9,12 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t size;
+	size_t size = 0;
 
 	if (!tree)
 		return (0);
 
-	size = tree->left ? 1 + binary_tree_size(tree->left) : 0;
-	size = tree->right ? 1 + binary_tree_size(tree->right) : 0;
-	size++;
-	return (size);
+	size += tree->left ? binary_tree_size(tree->left) : 0;
+	size += tree->right ? binary_tree_size(tree->right) : 0;
+	return (size + 1);
 }
